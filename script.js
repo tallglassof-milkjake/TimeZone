b06741c24535872a54ef8398bade59ec70c9aaa0
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-05-12 19:21:30 +0930

    made time dynamic, cleaned up script

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 $(function() {
-    let date = new Date();
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+    function createTime() {
+        let currentTime = new Date();
 
-    let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        let currentHour = currentTime.getHours();
+        let currentMinute = currentTime.getMinutes();
+        let currentSecond = currentTime.getSeconds();
+
+        currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
+        currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond;
+
+        let currentMeridiam = (currentHour < 12) ? "AM" : "PM";
+
+        currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
+
+        currentHour = (currentHour == 0) ? 12 : currentHour;
+
+        let currentTimeString = `${currentHour}:${currentMinute}:${currentSecond} ${currentMeridiam}`;
+
+        $("#currentTime").html(currentTimeString);
+    };
+
+    createTime();
+
+    setInterval(createTime, 1000);
+    
+
+    
+
+    let day = currentTime.getDate();
+    let month = currentTime.getMonth() + 1;
+    let year = currentTime.getFullYear();
 
     console.log(`${day}/${month}/${year} at ${time}`)
+
+    
 })
