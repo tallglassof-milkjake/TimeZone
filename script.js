0d312d2fff96bcf0d96279becbc556c5df5c40b4
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-05-19 13:07:38 +0930

    messing around with time manipulation using bootstrap 4 radios

--- script.js
+++ script.js
@@ -18,21 +18,64 @@ $(function() {
 
         let currentTimeString = `${currentHour}:${currentMinute}:${currentSecond} ${currentMeridiam}`;
 
-        $("#currentTime").html(currentTimeString);
+        if ($("#localTime").prop("checked", true)) {
+            $("#currentTime").html(currentTimeString);
+        }
+
+        // if ($("#localTime").prop("checked", false)) {
+        //     $("#localTime").attr("checked", false);
+        //     $("#currentTime").hide();
+        // } else {
+        //     $("#localTime").attr("checked", true);
+        //     $("#currentTime").html(currentTimeString);
+        // }
     };
 
     createTime();
 
     setInterval(createTime, 1000);
+
+    function timeZones() {
+        if ($("#otherTime").is(":checked")) {
+            $("#currentTime").html(currentTimeString).hide();
+            
+            let formDiv = '<div class="form-group mx-auto w-50">';
+            let formInput = '<input type="text" class="form-control" id="exampleFormControlInput1"></input>';
+            $("#timeZoneCheck").after(formDiv);
+            formDiv.append(formInput);
+        }
+    }
+
+    timeZones();
+
+    // function getCities() {
+
+    //     // let queryURL = "https://wft-geo-db.p.rapidapi.com/v1/locale/timezones/Australia__Sydney/time";
+
+    //     const settings = {
+    //         "async": true,
+    //         "crossDomain": true,
+    //         "url": "https://wft-geo-db.p.rapidapi.com/v1/locale/timezones/Australia__Sydney/time",
+    //         "method": "GET",
+    //         "headers": {
+    //             "x-rapidapi-key": "9412a276cbmsh4a03f98b46a37d7p1d53c7jsnbbec000e7aac",
+    //             "x-rapidapi-host": "wft-geo-db.p.rapidapi.com"
+    //         }
+    //     };
+        
+    //     $.ajax(settings).done(function (response) {
+    //         console.log(response);
+    //     });
+    // };
+
+    // getCities();
     
 
-    
+    // let day = currentTime.getDate();
+    // let month = currentTime.getMonth() + 1;
+    // let year = currentTime.getFullYear();
 
-    let day = currentTime.getDate();
-    let month = currentTime.getMonth() + 1;
-    let year = currentTime.getFullYear();
-
-    console.log(`${day}/${month}/${year} at ${time}`)
+    // console.log(`${day}/${month}/${year} at ${time}`)
 
     
 })
