d1f4b8a7fb6f92edf82ef1e7ac9b4879338f9f6b
Jake Mackinlay <jrmackinlay90@gmail.com>
2021-05-23 14:41:39 +0930

    toying around with timezones and formats

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ $(function() {
         currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
         currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond;
 
-        let currentMeridiam = (currentHour < 12) ? "AM" : "PM";
+        let currentMeridiam = (currentHour < 12) ? "am" : "pm";
 
         currentHour = (currentHour > 12) ? currentHour - 12 : currentHour;
 
         currentHour = (currentHour == 0) ? 12 : currentHour;
 
-        let currentTimeString = `${currentHour}:${currentMinute}:${currentSecond} ${currentMeridiam}`;
+        let currentTimeString = `${currentHour}:${currentMinute} ${currentMeridiam}`;
 
         if ($("#localTime").prop("checked", true)) {
             $("#currentTime").html(currentTimeString);
@@ -35,18 +35,18 @@ $(function() {
 
     setInterval(createTime, 1000);
 
-    function timeZones() {
-        if ($("#otherTime").is(":checked")) {
-            $("#currentTime").html(currentTimeString).hide();
+    // function timeZones() {
+    //     if ($("#otherTime").is(":checked")) {
+    //         $("#currentTime").html(currentTimeString).hide();
             
-            let formDiv = '<div class="form-group mx-auto w-50">';
-            let formInput = '<input type="text" class="form-control" id="exampleFormControlInput1"></input>';
-            $("#timeZoneCheck").after(formDiv);
-            formDiv.append(formInput);
-        }
-    }
+    //         let formDiv = '<div class="form-group mx-auto w-50">';
+    //         let formInput = '<input type="text" class="form-control" id="exampleFormControlInput1"></input>';
+    //         $("#timeZoneCheck").after(formDiv);
+    //         formDiv.append(formInput);
+    //     }
+    // }
 
-    timeZones();
+    // timeZones();
 
     function getCities() {
 
@@ -68,22 +68,38 @@ $(function() {
         //     console.log(response);
         // });
 
-        let date = new Date();
+        // let date = new Date();
         
-        let city = $("#checkBtn").click(function() {
+        $("#checkBtn").click(function() {
             
+            let date = new Date();
+
+            let foreignHour = date.getHours();
+            let foreignMinute = date.getMinutes();
+
+            foreignMinute = (foreignMinute < 10 ? "0" : "") + foreignMinute;
+            
+            let foreignMeridiam = (foreignHour < 12) ? "am" : "pm";
+
+            foreignHour = (foreignHour > 12) ? foreignHour - 12 : foreignHour;
+
+            foreignHour = (foreignHour == 0) ? 12 : foreignHour;
+
+            let foreignTime = `${foreignHour}:${foreignMinute} ${foreignMeridiam}`;
+
+
             let thisCity = $("#city").val();
             let thisCountry = $("#country").val();
             console.log(thisCountry);
             console.log(thisCity);
 
-            let dateAbroad = date.toLocaleString('en-US', {timeZone: `${thisCountry}/${thisCity}` });
+            let dateAbroad = date.toLocaleString('en-US', 'short', {timeZone: `${thisCountry}/${thisCity}` });
             console.log(dateAbroad)
 
             $("#selectedTimezone").html(dateAbroad);
         });
         
-        setInterval(city, 1000);
+        
         
     };
 
